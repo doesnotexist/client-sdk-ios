206deeca8fe1a7260bb6e2c6c2bed94ebdd9707a
Omar El-Domeiri <omar@doesnotexist.com>
2021-11-30 11:45:49 -0500

    restore changes to Package.swift

--- Package.swift
+++ Package.swift
@@ -4,44 +4,6 @@
 import PackageDescription
 
 let package = Package(
-<<<<<<< HEAD
-  name: "LiveKit",
-  platforms: [
-    .iOS(.v15),
-    .macOS(.v11)
-  ],
-  products: [
-    // Products define the executables and libraries a package produces, and make them visible to other packages.
-    .library(
-      name: "LiveKit",
-      targets: ["LiveKit"]
-    ),
-  ],
-  dependencies: [
-    .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", .upToNextMajor(from: "1.18.0")),
-    .package(name: "Promises", url: "https://github.com/google/promises.git", .upToNextMajor(from: "2.0.0")),
-    .package(url: "https://github.com/apple/swift-log.git", .upToNextMajor(from: "1.4.2")),
-    .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.0.1")),
-  ],
-  targets: [
-    .target(
-      name: "LiveKit",
-      dependencies: [
-        "WebRTC", "SwiftProtobuf", "Promises",
-        .product(name: "Logging", package: "swift-log"),
-        .product(name: "Collections", package: "swift-collections"),
-      ]
-    ),
-    .testTarget(
-      name: "LiveKitTests",
-      dependencies: ["LiveKit"]
-    ),
-    .binaryTarget(
-      name: "WebRTC",
-      path: "Frameworks/WebRTC.xcframework"
-    )
-  ]
-=======
     name: "LiveKit",
     platforms: [
         .iOS(.v13),
@@ -55,7 +17,6 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(name: "WebRTC", url: "https://github.com/webrtc-sdk/Specs.git", .exact("92.4515.10")),
         .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", .upToNextMajor(from: "1.18.0")),
         .package(name: "Promises", url: "https://github.com/google/promises.git", .upToNextMajor(from: "2.0.0")),
         .package(url: "https://github.com/apple/swift-log.git", .upToNextMajor(from: "1.4.2")),
@@ -71,10 +32,13 @@ let package = Package(
             ],
             path: "Sources"
         ),
+        .binaryTarget(
+            name: "WebRTC",
+            path: "Frameworks/WebRTC.xcframework"
+        ),
         .testTarget(
             name: "LiveKitTests",
             dependencies: ["LiveKit"]
         )
     ]
->>>>>>> main
 )
